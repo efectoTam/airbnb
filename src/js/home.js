92b7be27c4c8599044da33c8dd3792c7565b418c
nadiatapm <nadiatap@gmail.com>
2016-12-31 20:52:28 -0300

    arreglo import

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,5 @@
 //js seccionExperiencias
-/*var posicion = 0;
+var posicion = 0;
 	var imagenes = new Array();
 	$(document).ready(function() {
 		//alert(jQuery('.texto').html());
@@ -41,6 +41,28 @@
 		return false;
 	});
 
-});*/
+});
 
 /*date picker parte nadia */
+/*if ( $('[type="date"]').prop('type') != 'date' ) {
+} */
+
+if ( $('html').hasClass('no-touch') ) {
+	var $input, $btn;
+	$( ".date-wrapper" ).each(function( index ) {
+		$input = $(this).find('input');
+		$btn = $(this).find('.calendar-btn');
+		$input.attr('type', 'text');
+		var pickerStart = new Pikaday({
+			field: $input[0],
+			trigger: $btn[0],
+			container: $(this)[0],
+			format: 'DD/MM/YYYY',
+			firstDay: 1
+		});
+		$btn.show();
+	});
+} else {
+	$('.date-wrapper input').attr('type', 'date');
+	$('.calendar-btn').hide();
+}
