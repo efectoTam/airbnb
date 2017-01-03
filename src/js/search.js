df77196838fef6cbc462e11dca112ec4aaad3347
efectoTam <tam.luque@gmail.com>
2017-01-02 21:49:08 -0300

    Localstorage desde home a search

--- src/js/search.js
+++ src/js/search.js
@@ -1,2 +1,9 @@
 /*Localstorage que devuelve los datos de home*/
-$(".llegada-home").append(localStorage.getItem("fecha-llegada"));
+$(document).ready(function(){
+	var fechauno=localStorage.getItem("fecha-llegada");
+	$(".llegada-home").val(fechauno);
+	var fechados=localStorage.getItem("fecha-salida");
+	$(".salida-home").val(fechados);
+	var huesped=localStorage.getItem("num-huesped");
+	$(".huesped-home").val(huesped);
+});
